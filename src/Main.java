97ab06a94d96f5a5d758eec3a7dedc908ac9bbe3
Mateusz Czyż <matczy@gmail.com>
2016-10-11 14:09:53 +0200

    zadanie 2 i 3

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,38 @@
 import java.util.Scanner;
 public class Main {
-    void method() throws IOException { }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String n = "";
         try {
             System.out.print("podaj cene samochodu: ");
-            String n=scanner.nextLine();
+            n=scanner.nextLine();
             int cena = Integer.parseInt(n);
+            n="";
+            System.out.print("podaj moc samochodu: ");
+            n=scanner.nextLine();
+            int moc = Integer.parseInt(n);
+            n="";
+            System.out.print("podaj spalanie samochodu: ");
+            n=scanner.nextLine();
+            double spalanie = Double.parseDouble(n);
+            n="";
+            System.out.print("podaj kolor samochodu: ");
+            n=scanner.nextLine();
+            String kolor = n;
+            samochod vw = new samochod(moc, cena,kolor,spalanie);
+
+            System.out.println("VW cena: "+vw.cena);
+            System.out.println("Koszt przy 234 km: "+vw.paliwo(234,4.56));
         }
         catch (Exception e) {
-//obsluga wyjatkow klasy Exception
+            System.out.println("podales zle dane");
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
         }
 
-        n="";
-        System.out.print("podaj moc samochodu: ");
-        n=scanner.nextLine();
-        int moc = Integer.parseInt(n);
-        n="";
-        System.out.print("podaj spalanie samochodu: ");
-        n=scanner.nextLine();
-        double spalanie = Double.parseDouble(n);
-        n="";
-        System.out.print("podaj kolor samochodu: ");
-        n=scanner.nextLine();
-        String kolor = n;
-        samochod vw = new samochod(moc, cena,kolor,spalanie);
 
-        System.out.println("VW cena: "+vw.cena);
-        System.out.println("Koszt przy 234 km: "+vw.paliwo(234,4.56));
 
     }
 }
